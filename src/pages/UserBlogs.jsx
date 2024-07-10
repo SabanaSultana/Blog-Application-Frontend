5ee2448575f99f3370ba145189928f88bef6a81f
Sabana <sabanasultana132@gmail.com>
2024-07-10 22:34:14 +0530

    blog app frontend

---
+++ src/pages/UserBlogs.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import BlogCard from "../components/BlogCard";
+
+const UserBlogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState("");
+
+  //get user blogs
+  const getUserBlogs = async () => {
+    try {
+      const id = localStorage.getItem("userId");
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/blog/user-blog/${id}`
+      );
+      console.log(data); 
+      if (data?.success) {
+        setBlogs(data?.userBlog.blogs);
+        setUsername(data?.userBlog.username); 
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUserBlogs();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      {blogs && blogs.length > 0 ? (
+        blogs.map((blog) => (
+          <BlogCard
+            key={blog._id}
+            id={blog._id}
+            isUser={true}
+            title={blog.title}
+            description={blog.description}
+            image={blog.image}
+            username={username} 
+            time={blog.createdAt}
+          />
+        ))
+      ) : (
+        <h1>You haven't added any blog</h1>
+      )}
+    </div>
+  );
+};
+
+export default UserBlogs;
